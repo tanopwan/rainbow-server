3de72e485bc74e7adcc78ab9182403ff7a44b8cc
Tanopwan Tangprakit <tanopwan@gmail.com>
2018-07-23 10:47:32 +0700

    :turtle: readme.MD and rename package

--- main/main.go
+++ main/main.go
@@ -6,19 +6,19 @@ import (
 	"net/http"
 
 	_ "github.com/tanopwan/cookie-authentication/middleware"
-	"github.com/tanopwan/rainbow-server/server"
+	"github.com/tanopwan/rainbow-server/rainbow"
 )
 
 func main() {
 	log.Println("[main.go] started")
-	m1 := server.Middleware(func(h http.Handler) http.Handler {
+	m1 := rainbow.Middleware(func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			log.Printf("[log] middleware1 request\n")
 			h.ServeHTTP(w, r)
 			log.Printf("[log] middleware1 response\n")
 		})
 	})
-	m2 := server.Middleware(func(h http.Handler) http.Handler {
+	m2 := rainbow.Middleware(func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			log.Printf("[log] middleware2 request\n")
 			h.ServeHTTP(w, r)
@@ -26,7 +26,7 @@ func main() {
 		})
 	})
 
-	s := server.NewServer(":8081").UseRedis().RegisterMiddleware(m1).RegisterMiddleware(m2)
+	s := rainbow.NewServer(":8081").UseRedis().RegisterMiddleware(m1).RegisterMiddleware(m2)
 	s.DefaultMux().HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusOK)
